56b9fa000dd51945085a3489e70d8d0b4a19f9ce
Miri.Hid <mirihdz11@gmail.com>
2018-11-19 00:59:05 -0500

    Updated Changes 11.19.18

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
-//Create an array of animal strings & save it to a variable called animals
+    //Create an array of animal strings & save it to a variable called animals
      var animals=["panda", "hippo", "girraffe", "monkey", "lion", "tiger", "flamingo"]
      console.log(animals)
-//Function for displaying animal buttons
+    //Function for displaying animal buttons
     function renderButtons() {
-//Deleting the animal buttons prior to adding new animal buttons
+    //Deleting the animal buttons prior to adding new animal buttons
     $("#animal-button").empty();
-//Looping through the array of animals
+    //Looping through the array of animals
     for (var i = 0; i < animals.length; i++) {
-//Generating buttons for each animal in the array.
+    //Generating buttons for each animal in the array.
     var animalButton = $("<button>");
-//Adding a class of an-btn to animalButton variable
+    //Adding a class of an-btn to animalButton variable
     animalButton.addClass("an-btn");
-//Adding a data-attribute with a value of the animal at index i
+    //Adding a data-attribute with a value of the animal at index i
     animalButton.attr("data-name", animals[i]);
-//Providing the button's text with a value of the animal at index i
+    //Providing the button's text with a value of the animal at index i
     animalButton.text(animals[i]);
-//Adding the button to the HTML
+    //Adding the button to the HTML
     $("#animal-button").append(animalButton);
         }
     } 
     renderButtons(); 
     
 
-// displayAnimalInfo function re-renders the HTML to display the appropriate content
+    // displayAnimalInfo function re-renders the HTML to display the appropriate content
     $(document).on('click', '.an-btn', function() {
 
     var animal = $(this).attr("data-name");
@@ -38,21 +38,12 @@
         var results = response.data;
         var giphy = $("<img>");
         var viewGiphs = giphy.attr("src", results[0].images.fixed_height_still.url)
-        $('#giphy-view').append(viewGiphs)
-        
+        $('#giphy-view').append(viewGiphs)       
         });
-
-
-
-    // Adding a click event listener to all elements with a class of "an-btn"
-
-    // Calling the renderButtons function to display the intial button
     })
-
         // Function when button is clicked
-    //$("#animal-button").on("click", function(event) {
         $(document).on("click", "#add-animal", function(response){
-        // Preventing the buttons default behavior when clicked (which is submitting a form)
+        // Preventing the buttons default behavior when clicked
             event.preventDefault();
         // Grabs input from textbox
             var animalInput = $("#animals-input").val().trim();
@@ -69,5 +60,3 @@
     // Under every gif, display its rating (PG, G, so on).
     //    * This data is provided by the GIPHY API.
     //    * Only once you get images displaying with button presses should you move on to the next step.
-
-    //  Add a form to your page takes the value from a user input box and adds it into your `topics` array. Then make a function call that takes each topic in the array remakes the buttons on the page.
